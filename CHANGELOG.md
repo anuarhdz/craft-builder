16d2e00abfe5dc63bcc74c8d91ca4d6623f98d65
Anuar Reyes <anuar@hey.com>
2025-06-28 07:47:45 -0600

    feat: ✨ Add initial plugin structure and files
    * Introduced the `Builder` plugin with essential files
    * Added configuration for Composer, GitHub workflows, and coding standards
    * Included README, LICENSE, and CHANGELOG for documentation
    * Set up initial settings model and Twig template for plugin settings

---
+++ CHANGELOG.md
@@ -0,0 +1,4 @@
+# Release Notes for Builder
+
+## 1.0.0
+- Initial release
---
+++ src/models/Settings.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace anuarhdz\builder\models;
+
+use Craft;
+use craft\base\Model;
+
+/**
+ * Builder settings
+ */
+class Settings extends Model {}
